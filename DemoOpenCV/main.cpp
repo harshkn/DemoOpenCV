0f5a37bbbdb7be6775773213d806d9770ba1ec91
harshkn <harshkn@gmail.com>
2014-07-22 19:35:32 +0200

    Initial Commit

---
+++ DemoOpenCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DemoOpenCV
+//
+//  Created by Harsha K N on 22/07/14.
+//  Copyright (c) 2014 Harsha K N. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
