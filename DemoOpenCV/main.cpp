bf9d109293f3757844754cdd82908ced36e1ed6e
harshkn <harshkn@gmail.com>
2014-07-24 22:31:30 +0200

    Initial Commit. Simple opencv example to start.

--- DemoOpenCV/main.cpp
+++ DemoOpenCV/main.cpp
@@ -1,18 +1,25 @@
-//
-//  main.cpp
-//  DemoOpenCV
-//
-//  Created by Harsha K N on 22/07/14.
-//  Copyright (c) 2014 Harsha K N. All rights reserved.
-//
-
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, const char * argv[])
+int main(int argc, char *argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Open the file.
+    
+    IplImage *img = cvCreateImage( cvSize(100,200), IPL_DEPTH_8U, 3); //if (!img) {
+    
+    //    printf("Error: Couldn't open the image file.\n");
+    //    return 1;
+    //}
+    
+    // Display the image.
+    cvNamedWindow("Image:", CV_WINDOW_AUTOSIZE);
+    cvShowImage("Image:", img);
+    
+    // Wait for the user to press a key in the GUI window.
+    cvWaitKey(0);
+    // Free the resources.
+    cvDestroyWindow("Image:");
+    cvReleaseImage(&img);
+    
     return 0;
 }
-
